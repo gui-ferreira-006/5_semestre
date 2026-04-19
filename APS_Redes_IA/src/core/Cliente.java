81a31f45beead949dd05d5f070eddb3df2bc6833
meihuan90 <meihuanpaulo90@hotmail.com>
2026-04-19 16:48:42 -0300

    Anotações CLIENTE
    Feito todo um comentário no código para estar explicando o que está acontecendo e o que cada coisa faz

--- APS_Redes_IA/src/core/Cliente.java
+++ APS_Redes_IA/src/core/Cliente.java
@@ -1,16 +1,50 @@
 package core;
 
+/*
+
+Importações das Bibliotecas: 
+
+    import java.io.*; -> responsável por fazer o programa ler e escrever 
+    dados, manipulando arvquivos, diretórios e fluxos de dados.
+
+    import java.net.*; -> responsável pela comunicação entre computadores 
+    diferentes através de redes, permite que seu programa se conecte a
+    servidores, envie e receba dados pela internet, e crie conexões
+    de rede.
+
+*/
+
 import java.io.*;
 import java.net.*;
 
 public class Cliente {
 
+    /*
+    throws IOException -> avisa que esse método pode gerar erros de
+    entrada/saída e que não vai tratar eles aqui, só avisando que pode
+    acontecer.
+    */
+
     public static void main (String [] args) throws IOException {
 
+        /*
+        socket -> é a "porta" de comunicação entre o cliente e o servidor.
+        */
+        
         Socket socket = new Socket ("Localhost", 65173);
         System.out.println ("Conectado ao servidor com Sucesso!");
 
+        /*
+        PrintWriter -> Ferramenta de escrita de texto, ele pega qualquer
+        dado e transforma em texto para enviar a algum destino.
+        */
+
         PrintWriter saida = new PrintWriter (socket.getOutputStream (), true);
+
+        /*
+        BufferedReader -> Ferramenta de leitura de texto, recebe dados que
+        chegam pela rede e os tranforma em texto legível para o programa.
+        */
 
         BufferedReader entrada = new BufferedReader (new InputStreamReader (socket.getInputStream()));
 
