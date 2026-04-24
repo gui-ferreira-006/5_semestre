d3058c2871c3e915e0efa3a87409bfbd24aab2e2
meihuan90 <meihuanpaulo90@hotmail.com>
2026-04-24 12:30:23 -0300

    add Comentários
    Adição dos comentários dos respectivos códigos:
    
    -Servidor
    -ClienteHandler

--- APS_Redes_IA/src/core/ClienteHandler.java
+++ APS_Redes_IA/src/core/ClienteHandler.java
@@ -9,10 +9,11 @@ public class ClienteHandler implements Runnable {
     private Socket socket;
     private String nomeCliente;
 
-
+    //Lista compartilhada de todos os clientes conectados
     private static List <ClienteHandler> clientesConectados = new ArrayList<>();
     private PrintWriter saida;
 
+    //Construtor: recebe o socket do cliente que se conectou
     public ClienteHandler (Socket socket) {
         this.socket = socket;
 
@@ -24,14 +25,18 @@ public class ClienteHandler implements Runnable {
             BufferedReader entrada = new BufferedReader (new  InputStreamReader(socket.getInputStream()));
             saida = new PrintWriter (socket.getOutputStream(), true);
 
+            //Pede o nome do cliente assim que ele conecta e o armazena para idenficar suas mensagens
             saida.println ("Digite seu nome: ");
             nomeCliente = entrada.readLine();
             System.out.println (nomeCliente + " entrou no chat!");
 
+            //Adiciona esse cliente na lista de conectados
             clientesConectados.add(this);
 
+            //Avisa todos que o cliente entrou no chat
             enviarParaTodos (nomeCliente + " entrou no chat!");
 
+            //Fica em loop lendo mensagens enquanto o cliente estiver conectados
             String mensagem;   
             while ((mensagem = entrada.readLine ()) != null) {
                 System.out.println (nomeCliente + ": " + mensagem);
@@ -44,6 +49,7 @@ public class ClienteHandler implements Runnable {
             
         } finally {
 
+            //Remove o cliente da lista quando ele sair e avisa os outros que ele saiu do chat
             clientesConectados.remove(this);
             enviarParaTodos (nomeCliente + "saiu do chat.");
             try { socket.close (); } catch (IOException e) {}
@@ -51,6 +57,7 @@ public class ClienteHandler implements Runnable {
         }
     }
 
+    //Envia uma mensagem para todos os clientes conectados
     private void enviarParaTodos (String mensagem) {
         for (ClienteHandler cliente : clientesConectados) {
             cliente.saida.println(mensagem);
--- APS_Redes_IA/src/core/Servidor.java
+++ APS_Redes_IA/src/core/Servidor.java
@@ -8,22 +8,28 @@ public class Servidor {
 
     public static void main (String [] args) throws IOException {
 
-
+        //Abre uma "porta" para escutar as conexões dos clientes
         ServerSocket serverSocket = new ServerSocket (65173);
         System.out.println ("Aguardando conexão na porta 65173...");
 
+        //Fica esperando até um cliente realizar a conexão com a porta aberta
         Socket socket = serverSocket.accept();
         System.out.println ("Conectado ao servidor com sucesso!");
 
+        //Prepara para Ler mensagens do cliente
         BufferedReader entrada = new BufferedReader (new InputStreamReader (socket.getInputStream()));
 
+        //Prepara para enviar mensagens ao cliente
         PrintWriter saida = new PrintWriter (socket.getOutputStream (), true);
 
+        //Lê a mensagem que o cliente enviou
         String mensagem = entrada.readLine ();
         System.out.println ("You: " + mensagem);
 
+        //Responde ao cliente confirmando que a mensagem foi recebida
         saida.println ("Sua mensagem foi recebida com sucesso!");
 
+        //Fecha a conexão com o cliente e a porta do servidor
         socket.close ();
         serverSocket.close ();
 
